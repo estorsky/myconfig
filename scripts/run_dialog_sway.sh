22a7f996d980f73ca025186eb10b481e835d5b0b
Dmitry Privalov <estorsky@outlook.com>
2023-11-07 20:54:46 +0700

    updates

--- scripts/run_dialog_sway.sh
+++ scripts/run_dialog_sway.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-rofi -show combi &
+MONITOR="$(swaymsg -t get_outputs | jq -c '.[] | select(.focused) | select(.id)' | jq -r -c '.name')"
 
-swaymsg input "1:1:AT_Translated_Set_2_keyboard" xkb_switch_layout 0
+rofi -monitor "${MONITOR}" -show combi &
