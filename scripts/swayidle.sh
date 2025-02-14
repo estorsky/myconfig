1330da21fab29f15807d971fc4bc8bcff7ff453d
Dmitry Privalov <estorsky@outlook.com>
2025-02-14 20:40:26 +0700

    updates

--- scripts/swayidle.sh
+++ scripts/swayidle.sh
@@ -9,5 +9,5 @@ pidof -q swayidle && killall -9 swayidle
 swayidle -w \
     timeout "${TIMEOUT_LOCK_SEC}" 'swaylock.sh' \
     timeout "${TIMEOUT_SCREEN_SEC}" 'swaymsg "output * dpms off"' resume 'swaymsg "output * dpms on"' \
-    timeout "${TIMEOUT_SLEEP_SEC}" 'swaymsg "output * dpms on"; sleep 1; suspend.sh' \
+    timeout "${TIMEOUT_SLEEP_SEC}" '[ ! -d ~/work ] && (swaymsg "output * dpms on"; sleep 1; suspend.sh)' \
     before-sleep 'swaylock.sh'
