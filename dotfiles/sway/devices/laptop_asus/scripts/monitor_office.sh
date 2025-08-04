3cb99f79a1f4ff9ba5cf0b423eb5e782245cd748
Dmitry Privalov <estorsky@outlook.com>
2025-08-04 16:58:35 +0700

    updates

--- dotfiles/sway/devices/laptop_asus/scripts/monitor_office.sh
+++ dotfiles/sway/devices/laptop_asus/scripts/monitor_office.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-swaymsg output DP-2 position 0 0 mode 1920x1080@60.000Hz
+# TODO: fix pos
+swaymsg output eDP-1 position 0 0 mode 2560x1440@144.000Hz
+swaymsg output DP-2 position 2880 0 mode 1920x1080@60.000Hz
+swaymsg output DP-9 position 2880 0 mode 1920x1080@60.000Hz
+swaymsg output DP-10 position 4800 0 mode 1920x1080@60.000Hz
 swaymsg output eDP-1 disable
+swaymsg output DP-9 enable
+swaymsg output DP-10 enable
